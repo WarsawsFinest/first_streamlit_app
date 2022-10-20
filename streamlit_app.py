bbf7d09e0babf751bf643aaf983be6dbe7287b0b
WarsawsFinest <79460458+WarsawsFinest@users.noreply.github.com>
2022-10-20 07:57:41 -0500

    Update streamlit_app.py
    Normalize json data to be in a readable format 
    
    Convert normalized data to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalize json data to be in a readable format 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Convert normalized data to dataframe
+streamlit.dataframe(fruityvice_normalized)
